19d7feb3cbeb4c37f37df6855c52ab8407a8513e
AlanFontoura <alan.fontoura@d1g1t.com>
2025-09-09 21:37:21 -0400

    Delete client params file

--- fetools/client_params.py
+++
@@ -1,7 +0,0 @@
-CONSTANTS = {
-    "gresham": {
-        "custodian_bucket": "s3://d1g1t-custodian-data-us-east-1/apx/gresham",
-        "file_transfer_bucket": "s3://d1g1t-production-file-transfer-us-east-1/gresham"
-        }
-}
-
