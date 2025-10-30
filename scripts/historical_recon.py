ea18c98fd7fc857a4f3cbd6725c40e3b9ef26af8
AlanFontoura <alan.fontoura@d1g1t.com>
2025-10-30 16:26:55 -0400

    Start to write Historical Recon

---
+++ scripts/historical_recon.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import importlib
+
+
+class HistoricalRecon:
+    def __init__(self, client_name: str, start_date: str, end_date: str, tracking_data: pd.DataFrame, client_data: pd.DataFrame):
+        self.client_name = client_name
+        self.start_date = start_date
+        self.end_date = end_date
+        self.tracking_data = tracking_data
+        self.client_data = client_data
+
+    def filter_data_by_date(self, data: pd.DataFrame) -> pd.DataFrame:
+        return data[(data["Date"] >= self.start_date) & (data["Date"] <= self.end_date)]
+
+    def run(self) -> None:
+        module = importlib.import_module(self.module_name)
+        class_name = ''.join(word.title() for word in self.module_name.split('_'))
+        recon_class = getattr(module, class_name)(*self.args, **self.kwargs)
+        recon_class.main()
