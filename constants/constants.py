605fc71f7eb1ad16965c186772a9ea794327d492
AlanFontoura <alan.fontoura@d1g1t.com>
2026-01-05 19:53:06 +0000

    Major changes to repo structure

--- constants/constants.py
+++
@@ -1,7 +0,0 @@
-FILE_LOCATIONS = {
-    "gresham": {
-        "custodian_bucket": "s3://d1g1t-custodian-data-us-east-1/apx/gresham/",
-        "position_files": "s3://d1g1t-production-file-transfer-us-east-1/gresham/for_d1g1t/AllClientFiles/",
-        "exports_folder": "s3://d1g1t-client-us/gresham/exports/",
-    }
-}
--- tools/api_importer.py
+++
@@ -1,7 +0,0 @@
-# src/api_importer.py
-
-import pandas as pd
-
-
-class APIImporter:
-    pass
