0afce1c5b5d8295753d8984d4fd28e4d7ec400fc
AlanFontoura <alan.fontoura@d1g1t.com>
2026-01-08 17:08:07 +0000

    Recreate PO and SMA script

--- src/po_sma.py
+++ src/po_sma.py
@@ -1,192 +1,82 @@
 import pandas as pd
+from dataclasses import dataclass, asdict
+import json
 
 
-# region SMA
-class SMADataGenerator:
-    def __init__(self, sma_df: pd.DataFrame):
-        self.sma_df = sma_df
+@dataclass(frozen=True)
+class ColumnDefinitions:
+    SOURCE: str
+    COL_NAME: str | None = None
+    PREFIX: str | None = None
+    SUFFIX: str | None = None
+    VALUE: str | float | None = None
 
-    def generate_sma_data(self) -> None:
-        self.create_sma_funds()
-        self.create_sma_class_series()
-        self.create_sma_households()
-        self.create_sma_clients()
-        self.create_sma_accounts()
-        self.remap_sma_accounts()
-        self.create_sma_instruments()
-        self.create_sma_fund_client_ownership()
 
-    def create_sma_funds(self) -> None:
-        self.funds: pd.DataFrame = self.sma_df[
-            ["Account ID", "Account Name", "Currency", "Client ID"]
-        ].copy()
-        self.funds["Account ID"] = [
-            f"{fpk}_sma_fund" for fpk in self.funds["Account ID"]
-        ]
-        self.funds["Account Name"] = [
-            f"{name} - SMA Fund" for name in self.funds["Account Name"]
-        ]
-        self.funds["Type"] = "SMA"
-        self.funds = self.funds.rename(
-            columns={
-                "Account ID": "Firm Provided Key",
-                "Account Name": "Name",
-                "Client ID": "Fund Manager Firm Provided Key",
-            }
-        )
+@dataclass(frozen=True)
+class FileDefinitions:
+    FILENAME: str
+    OUTPUT_PATH: str
+    COLUMNS: list[ColumnDefinitions]
 
-    def create_sma_class_series(self) -> None:
-        self.class_series = self.sma_df[["Account ID", "Account Name"]].copy()
-        self.class_series["Account ID"] = [
-            f"{fpk}_sma_class_series"
-            for fpk in self.class_series["Account ID"]
-        ]
-        self.class_series["Account Name"] = [
-            f"{name} - SMA Class Series"
-            for name in self.class_series["Account Name"]
-        ]
-        self.class_series["Weight"] = 1.0
-        self.class_series["Fund Firm Provided Key"] = self.class_series[
-            "Account ID"
-        ].str.replace("_sma_class_series", "_sma_fund")
 
-        # TODO: Add columns below when supported
-        # self.class_series["Is Lookthrough Enabled"] = False
-        # self.class_series["Collapse when scaling down position"] = True
-        self.class_series = self.class_series.rename(
-            columns={
-                "Account ID": "Firm Provided Key",
-                "Account Name": "Name",
-            }
-        )
+class DataFrameChanger:
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
 
-    def create_sma_households(self) -> None:
-        self.households = self.sma_df[
-            ["Account ID", "Account Name", "Team Name"]
-        ].copy()
-        self.households["Account ID"] = [
-            f"{fpk}_sma_household" for fpk in self.households["Account ID"]
-        ]
-        self.households["Account Name"] = [
-            f"{name} - SMA Household"
-            for name in self.households["Account Name"]
-        ]
-        self.households = self.households.rename(
-            columns={
-                "Account ID": "Household ID",
-                "Account Name": "Household Name",
-            }
-        )
-
-    def create_sma_clients(self) -> None:
-        self.clients = self.sma_df[
-            [
-                "Account ID",
-                "Account Name",
-                "Jurisdiction",
-                "TaxResidency",
-                "Team Name",
+    def modify_column(
+        self,
+        column_def: ColumnDefinitions,
+    ) -> pd.DataFrame:
+        col_name = column_def.COL_NAME or column_def.SOURCE
+        if column_def.PREFIX:
+            self.df[col_name] = [
+                f"{column_def.PREFIX}{entry}"
+                for entry in self.df[column_def.SOURCE]
             ]
-        ].copy()
-        self.clients["Account ID"] = [
-            f"{fpk}_sma_client" for fpk in self.clients["Account ID"]
+        if column_def.SUFFIX:
+            self.df[col_name] = [
+                f"{entry}{column_def.SUFFIX}"
+                for entry in self.df[column_def.SOURCE]
+            ]
+        if column_def.VALUE is not None:
+            self.df[col_name] = column_def.VALUE
+        return self.df
+
+    def modify_columns(
+        self,
+        column_defs: list[ColumnDefinitions],
+        extra_columns: list[str] = [],
+    ) -> pd.DataFrame:
+        for column_def in column_defs:
+            self.df = self.modify_column(column_def)
+        keep_columns = [
+            column_def.COL_NAME if column_def.COL_NAME else column_def.SOURCE
+            for column_def in column_defs
+        ] + extra_columns
+        return self.df[keep_columns]
+
+
+class FileGenerator:
+    def __init__(self, data: pd.DataFrame, file_def: FileDefinitions):
+        self.data = DataFrameChanger(data)
+        self.file_def = file_def
+
+    def generate_file(self) -> pd.DataFrame:
+        self.data.df = self.data.modify_columns(self.file_def.COLUMNS)
+        return self.data.df
+
+
+def load_file_definitions(path: str) -> dict[str, FileDefinitions]:
+    with open(path, "r") as f:
+        raw_defs = json.load(f)
+    file_defs = {}
+    for key, value in raw_defs.items():
+        column_defs = [
+            ColumnDefinitions(**col_def) for col_def in value["COLUMNS"]
         ]
-        self.clients["Account Name"] = [
-            f"{name} - SMA Client" for name in self.clients["Account Name"]
-        ]
-        self.clients["ClientTypeDescription"] = "Foundation"
-        self.clients["Household ID"] = self.clients["Account ID"].str.replace(
-            "_sma_client", "_sma_household"
+        file_defs[key] = FileDefinitions(
+            FILENAME=value["FILENAME"],
+            OUTPUT_PATH=value["OUTPUT_PATH"],
+            COLUMNS=column_defs,
         )
-        self.clients["CRM_ContactID"] = self.clients[
-            "Account ID"
-        ].str.replace("_sma_client", "_sma_contact")
-        self.clients = self.clients.rename(
-            columns={
-                "Account ID": "ClientID",
-                "Account Name": "ClientName",
-            }
-        )
-
-    def create_sma_accounts(self) -> None:
-        self.accounts = self.sma_df.copy().drop(
-            columns=self.sma_df.filter(regex="^Asset").columns
-        )
-        self.accounts["Account ID"] = [
-            f"{fpk}_sma_account" for fpk in self.accounts["Account ID"]
-        ]
-        self.accounts["Account Name"] = [
-            f"{name} - SMA Account" for name in self.accounts["Account Name"]
-        ]
-        self.accounts["Account Type Name"] = "Other"
-
-    def remap_sma_accounts(self) -> None:
-        self.account_remap = self.sma_df[["Account ID"]].copy()
-        self.account_remap["Class Series ID"] = [
-            f"{fpk}_sma_class_series"
-            for fpk in self.account_remap["Account ID"]
-        ]
-        self.account_remap["Client ID"] = [
-            f"{fpk}_sma_client" for fpk in self.account_remap["Account ID"]
-        ]
-
-    def create_sma_instruments(self) -> None:
-        classification_cols = self.sma_df.filter(
-            regex="^Asset"
-        ).columns.tolist()
-        instrument_cols = [
-            "Instrument ID",
-            "Instrument Name",
-            "Currency Name",
-            "Valuation Per Position",
-            "Firm Security Type Name",
-            "Class Series ID",
-        ]
-        self.instruments = self.sma_df.copy()
-        self.instruments["Instrument ID"] = [
-            f"{fpk}_sma_instrument" for fpk in self.instruments["Account ID"]
-        ]
-        self.instruments["Instrument Name"] = [
-            f"{name} - SMA Instrument"
-            for name in self.instruments["Account Name"]
-        ]
-        self.instruments["Class Series ID"] = [
-            f"{fpk}_sma_class_series"
-            for fpk in self.instruments["Account ID"]
-        ]
-        self.instruments["Firm Security Type Name"] = "Unitless"
-        self.instruments["Valuation Per Position"] = True
-        self.instruments["Currency Name"] = self.instruments["Currency"]
-        self.instruments = self.instruments[
-            instrument_cols + classification_cols
-        ]
-
-    def create_sma_fund_client_ownership(self) -> None:
-        self.fund_client_ownership = self.sma_df[
-            ["Account ID", "Date Opened"]
-        ].copy()
-        self.fund_client_ownership["Class Series ID"] = [
-            f"{fpk}_sma_class_series"
-            for fpk in self.fund_client_ownership["Account ID"]
-        ]
-        self.fund_client_ownership["Client Account ID"] = [
-            f"{fpk}_sma_account"
-            for fpk in self.fund_client_ownership["Account ID"]
-        ]
-        self.fund_client_ownership["Percent"] = 1.0
-        self.fund_client_ownership = self.fund_client_ownership.rename(
-            columns={
-                "Date Opened": "Start Date",
-            }
-        )
-        self.fund_client_ownership = self.fund_client_ownership.drop(
-            columns=["Account ID"]
-        )
-
-
-# endregion
-
-# Partial Ownership
-
-
-# Join PO and SMA
+    return file_defs
