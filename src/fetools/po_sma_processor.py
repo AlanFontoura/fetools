9f182c2d1cc6a810fb3c4cf29e0818659a5e0df8
AlanFontoura <alan.fontoura@d1g1t.com>
2026-01-19 20:55:35 +0000

    Restructure owner inputs

--- src/fetools/po_sma_processor.py
+++ src/fetools/po_sma_processor.py
@@ -14,9 +14,8 @@ class Account:
     currency: str = ""
     udf1: str = ""
     udf2: str = ""
-    original_client_id: str = ""
-    original_client_name: str = ""
-    new_client_id: str | None = None
+    client_id: str = ""
+    client_name: str = ""
     sma_name: str | None = None
     sma_category: str | None = None
     sma_asset_class: str | None = None
@@ -24,16 +23,26 @@ class Account:
     sma_asset_class_l3: str | None = None
 
 
+@dataclass(frozen=True)
+class Owner:
+    id: str
+    name: str
+    ownership_structure: dict[
+        str, float
+    ]  # date to ownership percentage across dates
+
+
 class Structure:
-    def __init__(self, account: Account):
+    def __init__(self, account: Account, owners: list[Owner]):
         self.account = account
+        self.owners = owners
 
     def create_fund(self):
         return {
             "Firm Provided Key": f"{self.account.id}_fund",
             "Name": f"{self.account.name} - FUND",
             "Currency": self.account.currency,
-            "Fund Manager Firm Provided Key": self.account.original_client_id,
+            "Fund Manager Firm Provided Key": self.account.client_id,
             "Type": "SMA",
         }
 
@@ -64,6 +73,14 @@ class Structure:
             "CRM_ContactID": f"{self.account.id}_client",
         }
 
+    def update_client(self):
+        return {
+            "Client ID": self.account.client_id,
+            "User Defined 1": (
+                "Partially Owned" if self.account.is_partially_owned else None
+            ),
+        }
+
     def create_instrument(self):
         return {
             "Instrument ID": f"{self.account.id}_instrument",
@@ -85,7 +102,7 @@ class Structure:
             "Account ID": f"{self.account.id}_client",
             "Account Name": self.account.name,
             "Currency Name": self.account.currency,
-            "Client ID": self.account.original_client_id,
+            "Client ID": self.account.client_id,
             "Date Opened": self.account.opened_date,
             "Inception Date": self.account.opened_date,
             "Advisory Scope Name": self.account.advisory_scope,
@@ -101,3 +118,44 @@ class Structure:
             "Class Series ID": f"{self.account.id}_class",
             "Client ID": f"{self.account.id}_client",
         }
+
+    def fund_client_ownership(self):
+        return {
+            "Fund Firm Provided Key": f"{self.account.id}_fund",
+            "Client ID": f"{self.account.id}_client",
+            "Ownership Percentage": 1.0,
+        }
+
+
+class Split:
+    def __init__(self, account: Account, owner: Owner):
+        self.account = account
+        self.owner = owner
+        self.most_recent_date = max(owner.ownership_structure.keys())
+        self.pct_of_ownership = owner.ownership_structure[
+            self.most_recent_date
+        ]
+
+    def create_account(self):
+        return {
+            "Account Type Name": "Other",
+            "Account ID": f"{self.account.id}_{self.owner.id}",
+            "Account Name": f"{self.owner.name} {self.account.name} - {self.pct_of_ownership:.2%}",
+            "Client ID": self.owner.id,
+            "Date Opened": self.account.opened_date,
+            "Inception Date": self.account.opened_date,
+            "Advisory Scope Name": self.account.advisory_scope,
+            "Rep Code": self.account.rep_code,
+            "User Defined 1": self.account.udf1,
+            "User Defined 2": self.account.udf2,
+            "User Defined 5": "Split",
+        }
+
+    def create_fund_client_ownership(self):
+        for date, pct in self.owner.ownership_structure.items():
+            yield {
+                "Class Series ID": f"{self.account.id}_class",
+                "Client Account ID": f"{self.account.id}_client",
+                "Date": date,
+                "Ownership Percentage": pct,
+            }
